ea84a221533f2daf6d1a1a72723e4971e752bee8
euvitordev <euvitordev@gmail.com>
2024-06-13 17:09:13 -0300

    feat: add basic attributes

--- src/DataStructure/LinkedList.java
+++ src/DataStructure/LinkedList.java
@@ -1,6 +1,10 @@
 package DataStructure;
 
 public class LinkedList {
+    // Atributos
+    private Node head;
+    private Node tail;
+    private int size;
 
     // Nó do nosso método de LinkedList
     class Node {
