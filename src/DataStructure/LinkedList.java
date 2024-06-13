81c0922079d1705a6377a106547fed78aa423bbc
euvitordev <euvitordev@gmail.com>
2024-06-13 17:07:59 -0300

    feat: Add internal Node class to represent list nodes

--- src/DataStructure/LinkedList.java
+++ src/DataStructure/LinkedList.java
@@ -1,4 +1,15 @@
 package DataStructure;
 
 public class LinkedList {
+
+    // Nó do nosso método de LinkedList
+    class Node {
+        String data;
+        Node next;
+
+        // Construtor
+        Node(String data) {
+            this.data = data;
+        }
+    }
 }
