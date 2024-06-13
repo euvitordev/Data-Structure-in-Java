b699a125e8173bbaa6b16d06395c0cc4bda852ea
euvitordev <euvitordev@gmail.com>
2024-06-13 17:33:48 -0300

    Revert "add remove method to remove an element at a specific position from the list."
    This reverts commit 9603f8edf9575203785d8faca8483e5c64ee9df5.

--- src/DataStructure/LinkedList.java
+++ src/DataStructure/LinkedList.java
@@ -168,19 +168,4 @@ public class LinkedList {
         }
         return false;
     }
-
-    // Método para remover um elemento em uma posição específica da lista
-    public Node remove(int index) {
-        if (index < 0 || index >= size) return null;
-        if (index == 0) return removeFirst();
-        if (index == size - 1) return removeLast();
-
-        Node prev = get(index - 1);
-        Node temp = prev.next;
-        prev.next = temp.next;
-        temp.next = null;
-        size--;
-
-        return temp;
-    }
 }
